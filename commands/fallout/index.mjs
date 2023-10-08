d6d89c1a9335d18761274133e5deb2dacc39e8fc
Henry <henry.arcangello@proton.me>
2023-10-08 22:00:20 +0100

    update commands/fallout/index.mjs

--- commands/fallout/index.mjs
+++ commands/fallout/index.mjs
@@ -361,7 +361,7 @@ type([inicio, sobre, galeria, contato,idioma, terminal, logout ], { processChars
     terminal.remove();
 
     // Append the message and "Go Back" link to the 'outro' element
-type([iniciotext, goBackLinkIn], { processChars: false , wait: 10 }, outro);
+type([divinicio, goBackLinkIn], { processChars: false , wait: 10 }, outro);
     
 		
 		};
