4c12fab8a449f3480182587b5c72cc45dea1f3c7
Henry <henry.arcangello@proton.me>
2023-10-28 20:59:24 +0000

    update util/screens.js

--- util/screens.js
+++ util/screens.js
@@ -73,6 +73,9 @@ async function requestToChatGPT(inputText) {
 
   const result = await response.json();
   return result.choices[0].text
+  
+  
+}
 export function addClasses(el, ...cls) {
 	let list = [...cls].filter(Boolean);
 	el.classList.add(...list);
